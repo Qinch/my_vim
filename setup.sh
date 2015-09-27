6b8ecb36ce5640128e7d623f6d15cd4046ada983
qinchao <1187620726@qq.com>
2015-09-27 19:18:36 +0800

    add ctags

--- setup.sh
+++ setup.sh
@@ -6,16 +6,16 @@
 #!/bin/bash
 echo "安装将花费一定时间，请耐心等待直到安装完成^_^"
 
-sudo apt-get install -y cmake python-dev clang-format-3.6 
+sudo apt-get install -y cmake python-dev clang-format-3.6 ctags 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
-echo "正在努力为您安装bundle程序" > tmp
-echo "安装完毕将自动退出" >> tmp
-echo "请耐心等待" >> tmp
+echo "正在努力为您安装bundle程序" > qin
+echo "安装完毕将自动退出" >> qin
+echo "请耐心等待" >> qin
 
 cp vimrc ~/.vimrc
-vim tmp -c "BundleInstall" -c "q" -c "q"
-rm tmp
+vim qin  -c "BundleInstall" -c "q" -c "q"
+rm qin
 clang-format-3.6 -style=google -dump-config > ~/.clang-format 
 ~/.vim/bundle/YouCompleteMe/install.py --clang-completer
 cp ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/.ycm_extra_conf.py ~/
