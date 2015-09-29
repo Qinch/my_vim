8109b103528a2c1ad5cfbd687f2a5491f84c51d6
qinchao <1187620726@qq.com>
2015-09-29 13:04:56 +0800

    mod

--- setup.sh
+++ setup.sh
@@ -9,12 +9,12 @@ echo "安装将花费一定时间，请耐心等待直到安装完成^_^"
 sudo apt-get install -y  vim cmake python-dev clang-format-3.6 ctags 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
-echo "正在努力为您安装bundle程序" > qin
-echo "安装完毕将自动退出" >> qin
-echo "请耐心等待" >> qin
+echo "正在努力为您安装bundle程序" > tmp
+echo "安装完毕将自动退出" >> tmp
+echo "请耐心等待" >> tmp
 
 cp vimrc ~/.vimrc
-vim qin  -c "BundleInstall" -c "q" -c "q"
+vim tmp  -c "BundleInstall" -c "q" -c "q"
 rm qin
 clang-format-3.6 -style=google -dump-config > ~/.clang-format 
 ~/.vim/bundle/YouCompleteMe/install.py --clang-completer
